5cea83903a52c633121d6e1c8c4a3becf1ae13ef
fraser <fraser@DESKTOP-I7J85T1>
2019-10-22 11:38:23 -0500

    Add project files.

---
+++ ComponentConfig.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Runtime.InteropServices;
+using GME.Util;
+using GME.MGA;
+
+namespace GME.CSharp
+{
+
+    abstract class ComponentConfig
+    {
+        // Set paradigm name. Provide * if you want to register it for all paradigms.
+        public const string paradigmName = "CyPhyML";
+
+        // Set the human readable name of the addon. You can use white space characters.
+        public const string componentName = "Visualizer Integration";
+
+        // Select the object events you want the addon to listen to.
+        public const int eventMask = (int)(objectevent_enum.OBJEVENT_OPENMODEL | objectevent_enum.OBJEVENT_CLOSEMODEL);
+
+        // Uncomment the flag if your component is paradigm independent.
+        public static componenttype_enum componentType = componenttype_enum.COMPONENTTYPE_ADDON;
+
+        public const regaccessmode_enum registrationMode = regaccessmode_enum.REGACCESS_SYSTEM;
+        public const string progID = "MGA.Addon.VisualizerIntegration";
+        public const string guid = "39922EC7-3BDB-442D-AC21-D91C5F2893B1";
+    }
+}
---
+++ VisualizerIntegration.cs
@@ -0,0 +1,332 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.IO;
+using GME.CSharp;
+using GME.MGA;
+using GME;
+using GME.MGA.Core;
+using System.Threading;
+
+namespace VisualizerIntegration
+{
+    [Guid(ComponentConfig.guid),
+    ProgId(ComponentConfig.progID),
+    ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    public class VisualizerIntegrationAddon : IMgaComponentEx, IGMEVersionInfo, IMgaEventSink, IDisposable
+    {
+        private MgaAddOn addon;
+        private bool componentEnabled = true;
+        private bool handleEvents = true;
+        private MgaProject project = null;
+        private List<string> openModels = new List<string>();
+        private static string mgaFile = null;
+        private static List<string> modelsToReOpen = new List<string>();
+        GMEConsole GMEConsole { get; set; }
+
+        // Event handlers for addons
+        #region MgaEventSink members
+        public void GlobalEvent(globalevent_enum @event)
+        {
+            if (GMEConsole == null || GMEConsole.gme == null)
+            {
+                GMEConsole = GMEConsole.CreateFromProject(this.project);
+            }
+
+            if (@event == globalevent_enum.GLOBALEVENT_SAVE_PROJECT)
+            {
+                // update the list of models to be re-opened
+                modelsToReOpen = new List<string>(openModels);
+
+                // notify open models on console
+                GMEConsole.Error.Write(String.Format("Models Open: {0}", openModels.Count));
+                foreach(string model in openModels)
+                {
+                    GMEConsole.Out.Write(model);
+                }
+            }
+
+            if (@event == globalevent_enum.GLOBALEVENT_CLOSE_PROJECT)
+            {
+                // notify close project
+                if (GMEConsole != null)
+                {
+                    GMEConsole.Error.WriteLine("A project has been closed");
+                }
+
+                // properly destroy this object
+                if (GMEConsole != null)
+                {
+                    if (GMEConsole.gme != null)
+                    {
+                        Marshal.FinalReleaseComObject(GMEConsole.gme);
+                    }
+                    GMEConsole = null;
+                }
+                addon.Destroy();
+                Marshal.FinalReleaseComObject(addon);
+                addon = null;
+            }
+
+            if (@event == globalevent_enum.GLOBALEVENT_OPEN_PROJECT_FINISHED)
+            {
+                // notify open project
+                if (GMEConsole != null)
+                {
+                    GMEConsole.Out.WriteLine("A project has been opened");
+                }
+
+                // Re-opening a project, re-open the previously open models
+                if (project != null && mgaFile == project.ProjectConnStr)
+                {
+                    foreach (string objectPath in modelsToReOpen)
+                    {
+                        MgaObject obj = project.ObjectByPath[objectPath];
+                        try
+                        {
+                            GMEConsole.gme.ShowFCO((MgaFCO)obj, false);
+                        }
+                        catch (Exception e)
+                        {
+                            GMEConsole.Error.Write(e.ToString());
+                        }
+                    }
+                    modelsToReOpen.Clear();
+                }
+
+                // update the open mga file name
+                mgaFile = GMEConsole.gme.MgaProject.ProjectConnStr;
+            }
+
+            #region Other EventHandlers
+            if (@event == globalevent_enum.APPEVENT_XML_IMPORT_BEGIN)
+            {
+                handleEvents = false;
+                addon.EventMask = 0;
+            }
+            else if (@event == globalevent_enum.APPEVENT_XML_IMPORT_END)
+            {
+                unchecked { addon.EventMask = (uint)ComponentConfig.eventMask; }
+                handleEvents = true;
+            }
+            else if (@event == globalevent_enum.APPEVENT_LIB_ATTACH_BEGIN)
+            {
+                addon.EventMask = 0;
+                handleEvents = false;
+            }
+            else if (@event == globalevent_enum.APPEVENT_LIB_ATTACH_END)
+            {
+                unchecked { addon.EventMask = (uint)ComponentConfig.eventMask; }
+                handleEvents = true;
+            }
+            #endregion
+
+            if (!componentEnabled)
+            {
+                return;
+            }
+
+            // TODO: Handle global events
+            // MessageBox.Show(@event.ToString());
+        }
+
+        /// <summary>
+        /// Called when an FCO or folder changes
+        /// </summary>
+        /// <param name="subject">the object the event(s) happened to</param>
+        /// <param name="eventMask">objectevent_enum values ORed together</param>
+        /// <param name="param">extra information provided for cetertain event types</param>
+        public void ObjectEvent(MgaObject subject, uint eventMask, object param)
+        {
+            if (!componentEnabled || !handleEvents)
+            {
+                return;
+            }
+            if (GMEConsole == null)
+            {
+                GMEConsole = GMEConsole.CreateFromProject(subject.Project);
+            }
+            if ((eventMask & (uint)objectevent_enum.OBJEVENT_OPENMODEL) != 0)
+            {
+                openModels.Add(subject.AbsPath);
+                GMEConsole.Error.Write(String.Format("Opened Model: {0}", subject.AbsPath));
+            }
+            if ((eventMask & (uint)objectevent_enum.OBJEVENT_CLOSEMODEL) != 0)
+            {
+                openModels.Remove(subject.AbsPath);
+                GMEConsole.Error.Write(String.Format("Closed Model: {0}", subject.AbsPath));
+            }
+
+            // TODO: Handle object events (OR eventMask with the members of objectevent_enum)
+            // Warning: Only those events are received that you have subscribed for by setting ComponentConfig.eventMask
+
+            // If the event is OBJEVENT_DESTROYED, most operations on subject will fail
+            //   Safe operations: getting Project, ObjType, ID, MetaRole, Meta, MetaBase, Name, AbsPath
+            //   Operations that will fail: all others, including attribute access and graph navigation
+            //     Try handling OBJEVENT_PRE_DESTROYED if these operations are necessary
+
+            // Be careful not to modify Library objects (check subject.IsLibObject)
+
+            // MessageBox.Show(eventMask.ToString());
+            // GMEConsole.Out.WriteLine(subject.Name);
+
+        }
+
+        #endregion
+
+        #region IMgaComponentEx Members
+
+        public void Initialize(MgaProject project)
+        {
+            // Creating addon
+            project.CreateAddOn(this, out addon);
+            this.project = project;
+            // Setting event mask (see ComponentConfig.eventMask)
+            unchecked
+            {
+                addon.EventMask = (uint)ComponentConfig.eventMask;
+            }
+        }
+
+        public void InvokeEx(MgaProject project, MgaFCO currentobj, MgaFCOs selectedobjs, int param)
+        {
+            throw new NotImplementedException(); // Not called by addon
+        }
+
+
+        #region Component Information
+        public string ComponentName
+        {
+            get { return GetType().Name; }
+        }
+
+        public string ComponentProgID
+        {
+            get
+            {
+                return ComponentConfig.progID;
+            }
+        }
+
+        public componenttype_enum ComponentType
+        {
+            get { return ComponentConfig.componentType; }
+        }
+        public string Paradigm
+        {
+            get { return ComponentConfig.paradigmName; }
+        }
+        #endregion
+
+        #region Enabling
+        bool enabled = true;
+        public void Enable(bool newval)
+        {
+            enabled = newval;
+        }
+        #endregion
+
+        #region Interactive Mode
+        protected bool interactiveMode = true;
+        public bool InteractiveMode
+        {
+            get
+            {
+                return interactiveMode;
+            }
+            set
+            {
+                interactiveMode = value;
+            }
+        }
+        #endregion
+
+        #region Custom Parameters
+        SortedDictionary<string, object> componentParameters = null;
+
+        public object get_ComponentParameter(string Name)
+        {
+            if (Name == "type")
+                return "csharp";
+
+            if (Name == "path")
+                return GetType().Assembly.Location;
+
+            if (Name == "fullname")
+                return GetType().FullName;
+
+            object value;
+            if (componentParameters != null && componentParameters.TryGetValue(Name, out value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+
+        public void set_ComponentParameter(string Name, object pVal)
+        {
+            if (componentParameters == null)
+            {
+                componentParameters = new SortedDictionary<string, object>();
+            }
+
+            componentParameters[Name] = pVal;
+        }
+        #endregion
+
+        #region Unused Methods
+        // Old interface, it is never called for MgaComponentEx interfaces
+        public void Invoke(MgaProject Project, MgaFCOs selectedobjs, int param)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Not used by GME
+        public void ObjectsInvokeEx(MgaProject Project, MgaObject currentobj, MgaObjects selectedobjs, int param)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #endregion
+
+        #region IMgaVersionInfo Members
+
+        public GMEInterfaceVersion_enum version
+        {
+            get { return GMEInterfaceVersion_enum.GMEInterfaceVersion_Current; }
+        }
+
+        #endregion
+
+        #region Registration Helpers
+
+        [ComRegisterFunctionAttribute]
+        public static void GMERegister(Type t)
+        {
+            Registrar.RegisterComponentsInGMERegistry();
+        }
+
+        [ComUnregisterFunctionAttribute]
+        public static void GMEUnRegister(Type t)
+        {
+            Registrar.UnregisterComponentsInGMERegistry();
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            if (addon != null)
+            {
+                addon.Destroy();
+                addon = null;
+            }
+        }
+    }
+}
